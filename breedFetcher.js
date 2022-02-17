e480fd368754a655298c1f6b70520dfd427d645f
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 10:02:11 -0500

    Implemented functionality to retrieve description from hard coded cat breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,7 @@
 const request = require('request');
+const {inspect} = require('util');
+
+request('https://api.thecatapi.com/v1/images/search?breed_id=sibe', (err, response, body) => {
+  const {description} = JSON.parse(body)[0].breeds[0];
+  console.log(description);
+});
