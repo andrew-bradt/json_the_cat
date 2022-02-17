f9511c7ab01d22a66fd2baad659412eea0a32475
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 10:03:39 -0500

    accept command line arg to pass in a breed name to api req

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,7 +1,8 @@
 const request = require('request');
 const {inspect} = require('util');
+const breed = process.argv[2];
 
-request('https://api.thecatapi.com/v1/images/search?breed_id=sibe', (err, response, body) => {
+request(`https://api.thecatapi.com/v1/images/search?breed_id=${breed}`, (err, response, body) => {
   const {description} = JSON.parse(body)[0].breeds[0];
   console.log(description);
 });
