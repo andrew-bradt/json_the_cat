d58ef31abbc7b49915d5703f054cccd654221589
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 13:48:00 -0500

    implemented edge case - if statusCode is nto 200, return data.message

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,14 @@
 const request = require('request');
-const {inspect} = require('util');
-const breed = process.argv[2].toLowerCase();
 
-if (!breed) return console.log('Please Enter a Breed Name in the Console.');
+const arg = process.argv[2];
+if (!arg) return console.log('Please Enter a Breed Name in the Console.');
 
+const breed = arg.toLowerCase();
 request(`https://api.thecatapi.com/v1/images/search?breed_id=${breed}`, (err, response, body) => {
   if (err) return console.log(err);
+  const data = JSON.parse(body);
+  if (response.statusCode !== 200) return console.log(data.message);
   if (body === '[]') return console.log('Breed Name Not Found');
-  const {description} = JSON.parse(body)[0].breeds[0];
+  const {description} = data[0].breeds[0];
   console.log(description);
 });
