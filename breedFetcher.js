83e604964742182511ef5330927cca0c22405722
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 10:14:59 -0500

    Implemented edge cases: no breed name entered, breed name not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,10 @@ const request = require('request');
 const {inspect} = require('util');
 const breed = process.argv[2];
 
+if (!breed) return console.log('Please Enter a Breed Name in the Console.');
 request(`https://api.thecatapi.com/v1/images/search?breed_id=${breed}`, (err, response, body) => {
+  if (err) return console.log(err);
+  if (body === '[]') return console.log('Breed Name Not Found');
   const {description} = JSON.parse(body)[0].breeds[0];
   console.log(description);
 });
