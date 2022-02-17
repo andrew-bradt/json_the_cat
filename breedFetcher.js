549e07501d977a236e587154ecdd25b3908f37d9
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 10:20:22 -0500

    Implemented edge case: if domain name is incorrect, print error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,9 @@
 const request = require('request');
 const {inspect} = require('util');
-const breed = process.argv[2];
+const breed = process.argv[2].toLowerCase();
 
 if (!breed) return console.log('Please Enter a Breed Name in the Console.');
+
 request(`https://api.thecatapi.com/v1/images/search?breed_id=${breed}`, (err, response, body) => {
   if (err) return console.log(err);
   if (body === '[]') return console.log('Breed Name Not Found');
